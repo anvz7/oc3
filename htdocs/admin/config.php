9e13f17ab3804895d2617d68c2e51e25f7f4ef69
Завалин Андрей <zavalin@corpcentre.ru>
2022-12-29 19:02:14 +0400

    all files

---
+++ htdocs/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://imt18.loc/admin/');
+define('HTTP_CATALOG', 'http://imt18.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://imt18.loc/admin/');
+define('HTTPS_CATALOG', 'http://imt18.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/shared/httpd/imt18/htdocs/admin/');
+define('DIR_SYSTEM', '/shared/httpd/imt18/htdocs/system/');
+define('DIR_IMAGE', '/shared/httpd/imt18/htdocs/image/');
+define('DIR_STORAGE', '/shared/httpd/imt18/storage/');
+define('DIR_CATALOG', '/shared/httpd/imt18/htdocs/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc3032');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
